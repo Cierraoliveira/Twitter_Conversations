5acfb2bb8fbb33d59183edeb858603622f02ef10
Cierraoliveira <60296493+Cierraoliveira@users.noreply.github.com>
2020-06-04 11:29:27 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'Twitter_Conversations',         # How you named your package folder (MyLib)
   packages = ['Twitter_Conversations'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
-  # license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This scrapes twitter conversations',   # Give a short description about your library
   author = 'Cierra Oliveira',                   # Type in your name
   author_email = 'cierraoliveira@gmail.com',      # Type in your E-Mail
@@ -18,8 +18,8 @@ setup(
     'Development Status :: 5 - Production/Stable',  
     'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
-   # 'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
